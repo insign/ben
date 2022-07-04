8e3631d67f6c82be648cd7de362bf2b45d137f97
Hélio <insign@gmail.com>
2022-07-04 05:03:45 -0300

    fix #2

--- src/dns_command.sh
+++ src/dns_command.sh
@@ -11,6 +11,8 @@ get_split() {
 	echo "$string" | cut -d"$separator" -f"$position"
 }
 
+int(){ printf '%d' ${1:-} 2>/dev/null || :; }
+
 # shellcheck disable=SC2154
 for ((round = 1; round < ${args[--rounds]} + 1; round++)); do
 	echo "Round $round"
@@ -23,9 +25,11 @@ for ((round = 1; round < ${args[--rounds]} + 1; round++)); do
 			server_name=${dns[1]}
 			# shellcheck disable=SC2154
 			server_time=$(dig @"${server_ip}" -q "${args[host]}" +noall +stats | sed -nEz 's/.*;; Query time:\s([^\n]*).*.msec.*/\1/p')
+			server_time=$(int $server_time)
+
 
 			printf "%15s%12s%5dms\n" "$server_ip" "$server_name" "$server_time"
-			if [ "${server_time}" -lt ${lowest_server_time%ms} ]; then
+			if [ "${server_time}" -lt $lowest_server_time ]; then
 				lowest_server_time=$server_time
 				lowest_server_ip=$server_ip
 				lowest_server_name=$server_name
